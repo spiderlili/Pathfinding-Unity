0c262c4992788e19905bf4bd2bfff6169051f4cd
Jing Tan <jtan1993@gmail.com>
2018-07-01 23:49:46 +0100

    node class added

---
+++ Assets/Scripts/MapData.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapData : MonoBehaviour {
+
+    public int width = 10;
+    public int height = 5;
+
+    void Start () {
+        //int[,] mapInstance = MakeMap();
+	}
+
+    public int[,] MakeMap() {
+        //temp array
+        int[,] map = new int[width, height];
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                //default to open 0
+                map[x, y] = 0;
+            }
+        }
+        //add blocked walls 1 and leave open spaces 0 connected to draw path from 1 part of the maze to another
+        map[1, 0] = 1;
+        map[1, 1] = 1;
+        map[1, 2] = 1;
+
+        return map;
+    }
+}
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//open to be freely walked upon or blocked by a wall - freely accessible to any other object
+public enum NodeType {
+    Open = 0,
+    Blocked = 1
+}
+
+//data container
+public class Node {
+    public NodeType nodetype = NodeType.Open; //freely walkable by default
+    //identify each node by an index, use a 2D array to keep track of all the nodes - the node class has properties to store x and y index
+    public int xIndex = -1; //track position in 2D array initialised to an invalid value as a flag(when index has not been fit)
+    public int yIndex = -1;
+
+    //store coordinates that directly correspond to x and y
+    public Vector3 position; //keep track of the node's position, no transform data available from monobehaviour
+
+    //keep track of nodes connected to this node, use list as connection is based on proximity
+    public List<Node> neighbours = new List<Node>();
+    //special node to reference the best path through the maze to get to this node, each node keeps track of this best path to get to it
+
+    public Node previous = null; 
+    //constructor to set up the basics - save parameters to corresponding variables
+    public Node(int xIndex, int yIndex, NodeType nodeType) {
+        this.xIndex = xIndex;
+        this.yIndex = yIndex;
+        this.nodetype = nodeType;
+    }
+
+    //clear previous pathfinding attempts - rebuild/recalculate the path 
+    public void Reset() {
+        previous = null;
+    }
+}
